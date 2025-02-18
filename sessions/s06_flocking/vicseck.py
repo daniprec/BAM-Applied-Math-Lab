383573cf789feec2a688b1fe039bc42c076cb6e0
Daniel Precioso <danipg1409@gmail.com>
2025-02-18 20:14:50 +0100

    Make Vicseck model work

--- sessions/s06_flocking/vicseck.py
+++ sessions/s06_flocking/vicseck.py
@@ -4,117 +4,123 @@ import numpy as np
 import scipy.spatial
 
 
-def initialize_cells(n: int, d: float = 25) -> np.ndarray:
+def initialize_particles(n: int, d: float = 25) -> tuple[np.ndarray, np.ndarray]:
     """
-    Initialize the state of the cells.
+    Initialize the state of the particles.
 
     Parameters
     ----------
     n : int
-        Number of cells.
+        Number of particles.
     d : float, optional
         Dimension of the space (default is 25).
 
     Returns
     -------
     np.ndarray
-        Initial state vector of the cells.
+        Initial positions of the particles.
+    np.ndarray
+        Initial angle of the particles, in radians.
     """
-    # Random initial angles
-    angles = np.random.uniform(0, 2 * np.pi, n)
+    # Random initial theta
+    theta = np.random.uniform(0, 2 * np.pi, n)
     # Random initial x, y xy
     xy = np.random.uniform(0, d, (2, n))
-    return xy, angles
+    return xy, theta
 
 
 def update_rule(
     xy: np.ndarray,
-    angles: np.ndarray,
+    theta: np.ndarray,
     dt: float = 1,
     eta: float = 0.1,
     d: float = 25,
     r: float = 1,
-    v0: float = 2,
-) -> np.ndarray:
+    v0: float = 0.03,
+) -> tuple[np.ndarray, np.ndarray]:
     """
-    Update the state of the cells based on the Vicsek model.
+    Update the state of the particles based on the Vicsek model.
 
     Parameters
     ----------
-    present : np.ndarray
-        Current state vector of the cells.
+    xy : np.ndarray
+        Position of the particles.
+    theta : np.ndarray
+        Angle of the particles.
     dt : float, optional
-        Step size for the update (default is 1).
+        Time step, default is 1 (standard convention).
     eta : float, optional
-        Noise parameter (default is 0.1).
+        Noise parameter, default is 0.1.
     d : float, optional
-        Dimension of the space (default is 25).
+        Dimension of the space, default is 25.
     r : float, optional
-        Interaction radius (default is 1).
+        Interaction radius, default is 1 (standard convention).
     v0 : float, optional
-        Speed of the cells (default is 0.03).
+        Speed of the particles, default is 0.03.
 
     Returns
     -------
     np.ndarray
-        Updated state vector of the cells.
+        Updated position of the particles.
+    np.ndarray
+        Updated angle of the particles.
     """
     # Compute distance matrix and neighbor matrix
     d_matrix = scipy.spatial.distance.pdist(xy.T)
     d_matrix = scipy.spatial.distance.squareform(d_matrix)
     neighbors = d_matrix <= r
-    neighbor_mean_angles = np.zeros_like(angles)
-
-    for i in range(len(angles)):
-        # Compute mean angle of neighbors
-        neighbor_mean_angles[i] = np.mean(angles[neighbors[:, i]])
-    dtheta = neighbor_mean_angles - angles
-    angles = np.mod(angles + dtheta * dt, 2 * np.pi)
+    # Compute mean angle of neighbors
+    neighbor_mean_theta = theta @ neighbors / np.sum(neighbors, axis=1)
+    # Update angle
+    theta = neighbor_mean_theta + np.random.uniform(-eta / 2, eta / 2, len(theta))
 
     # Update position
-    v = v0 * np.array([np.cos(angles), np.sin(angles)])
+    v = v0 * np.array([np.cos(theta), np.sin(theta)])
     xy = xy + dt * v
     xy = np.mod(xy, d)
 
-    return xy, angles
+    return xy, theta
 
 
-def plot_cells(xy: np.ndarray, angles: np.ndarray, d: float = 25):
+def plot_particles(xy: np.ndarray, theta: np.ndarray, d: float = 25):
     """
-    Plot the cells on a 2D plane.
+    Plot the particles in the 2D space.
 
     Parameters
     ----------
-    state_vector : np.ndarray
-        State vector of the cells.
+    xy : np.ndarray
+        Position of the particles.
+    theta : np.ndarray
+        Angle of the particles.
     d : float, optional
-        Dimension of the space (default is 25).
+        Dimension of the space, default is 25.
     """
     ax = plt.gca()
     ax.cla()
     ax.set_xlim(0, d)
     ax.set_ylim(0, d)
-    ar_scale = d / 250
-    for i in range(len(angles)):
-        ax.arrow(
-            xy[0, i],
-            xy[1, i],
-            5 * ar_scale * np.cos(angles[i]),
-            5 * ar_scale * np.sin(angles[i]),
-            head_width=2 * ar_scale,
-            head_length=ar_scale,
-            fc="k",
-            ec="k",
-        )
+
+    # Plot using a quiver plot
+    ax.quiver(
+        xy[0],
+        xy[1],
+        np.cos(theta),
+        np.sin(theta),
+        angles="xy",
+        scale_units="xy",
+        scale=d / 20,
+    )
     plt.draw()
 
 
 def run_animation(
-    n: int = 30,
-    dt: float = 0.01,
+    n: int = 300,
+    v0: float = 0.03,
+    dt: float = 1,
     r: float = 1,
     d: float = 25,
     eta: float = 0.1,
+    niter: int = 3,
 ):
     """
     Run the animation of the Vicsek model.
@@ -125,28 +131,30 @@ def run_animation(
     Parameters
     ----------
     n : int, optional
-        Number of cells (default is 30).
-    t : int, optional
-        Number of time steps (default is 1000).
-    r : float, optional
-        Interaction radius (default is 1).
-    d : float, optional
-        Dimension of the space (default is 25).
-    eta : float, optional
-        Noise parameter (default is 0.1).
+        Number of particles, default is 300.
+    v0 : float, optional
+        Speed of the particles, default is 0.03.
     dt : float, optional
-        Step size for the update (default is 1).
+        Time step, default is 1 (standard convention).
+    r : float, optional
+        Interaction radius, default is 1 (standard convention).
+    d : float, optional
+        Dimension of the space, default is 25.
+    eta : float, optional
+        Noise parameter, default is 0.1.
+    niter : int, optional
+        Number of iterations per frame, default is 3.
     """
-    xy, positions = initialize_cells(n, d=d)
+    xy, positions = initialize_particles(n, d=d)
 
     def update_animation(frame: int):
         nonlocal xy, positions
-        for _ in range(10):
-            xy, positions = update_rule(xy, positions, dt=dt, r=r, d=d, eta=eta)
-        plot_cells(xy, positions, d=d)
+        for _ in range(niter):
+            xy, positions = update_rule(xy, positions, v0=v0, dt=dt, r=r, d=d, eta=eta)
+        plot_particles(xy, positions, d=d)
 
     fig, ax = plt.subplots()
-    ani = animation.FuncAnimation(fig, update_animation, interval=1)
+    ani = animation.FuncAnimation(fig, update_animation, interval=0)
     plt.show()
 
 
