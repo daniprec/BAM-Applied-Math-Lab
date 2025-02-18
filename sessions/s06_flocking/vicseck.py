5e611476342bc2a738f68f520b79c994f9ffb54c
Daniel Precioso <danipg1409@gmail.com>
2025-02-18 19:47:15 +0100

    Update Vicseck model

--- sessions/s06_flocking/vicseck.py
+++ sessions/s06_flocking/vicseck.py
@@ -20,20 +20,21 @@ def initialize_cells(n: int, d: float = 25) -> np.ndarray:
     np.ndarray
         Initial state vector of the cells.
     """
-    state0 = np.zeros(shape=(n * 3,))
-    state0[0::3] = np.random.rand(n) * 2 * np.pi  # Random initial angles
-    state0[1::3] = np.random.rand(n) * d  # Random initial x positions
-    state0[2::3] = np.random.rand(n) * d  # Random initial y positions
-    return state0
+    # Random initial angles
+    angles = np.random.uniform(0, 2 * np.pi, n)
+    # Random initial x, y xy
+    xy = np.random.uniform(0, d, (2, n))
+    return xy, angles
 
 
 def update_rule(
-    present: np.ndarray,
-    stepsize: float = 1,
+    xy: np.ndarray,
+    angles: np.ndarray,
+    dt: float = 1,
     eta: float = 0.1,
     d: float = 25,
     r: float = 1,
-    v0: float = 0.03,
+    v0: float = 2,
 ) -> np.ndarray:
     """
     Update the state of the cells based on the Vicsek model.
@@ -42,7 +43,7 @@ def update_rule(
     ----------
     present : np.ndarray
         Current state vector of the cells.
-    stepsize : float, optional
+    dt : float, optional
         Step size for the update (default is 1).
     eta : float, optional
         Noise parameter (default is 0.1).
@@ -58,44 +59,27 @@ def update_rule(
     np.ndarray
         Updated state vector of the cells.
     """
-    future = present.copy()
-    n = np.size(present) // 3
-    neighbor_mean_angles = np.zeros(n)
-
-    positions = np.zeros((n, 2))
-    positions[:, 0] = present[1::3]
-    positions[:, 1] = present[2::3]
-    angles = present[0::3]
-
     # Compute distance matrix and neighbor matrix
-    d_matrix = scipy.spatial.distance.pdist(positions)
+    d_matrix = scipy.spatial.distance.pdist(xy.T)
     d_matrix = scipy.spatial.distance.squareform(d_matrix)
     neighbors = d_matrix <= r
+    neighbor_mean_angles = np.zeros_like(angles)
 
-    for i in range(n):
-        my_theta = angles[i]
-        my_pos = positions[i, :]
-
+    for i in range(len(angles)):
         # Compute mean angle of neighbors
-        neighbor_mean_angles[i] = np.sum(angles[neighbors[:, i]]) / np.sum(
-            neighbors[:, i]
-        )
+        neighbor_mean_angles[i] = np.mean(angles[neighbors[:, i]])
+    dtheta = neighbor_mean_angles - angles
+    angles = np.mod(angles + dtheta * dt, 2 * np.pi)
 
-        # Update position
-        v = v0 * np.array([np.cos(my_theta), np.sin(my_theta)])
-        future[np.array([1, 2]) + 3 * i] = (
-            present[np.array([1, 2]) + 3 * i] + stepsize * v
-        )
-        future[np.array([1, 2]) + 3 * i] = np.mod(future[np.array([1, 2]) + 3 * i], d)
+    # Update position
+    v = v0 * np.array([np.cos(angles), np.sin(angles)])
+    xy = xy + dt * v
+    xy = np.mod(xy, d)
 
-    # Add noise to the angles
-    noise = -eta / 2 + np.random.rand(n) * eta / 2
-    future[0::3] = np.mod(present[0::3] + neighbor_mean_angles + noise, 2 * np.pi)
-
-    return future
+    return xy, angles
 
 
-def plot_cells(state_vector: np.ndarray, d: float = 25):
+def plot_cells(xy: np.ndarray, angles: np.ndarray, d: float = 25):
     """
     Plot the cells on a 2D plane.
 
@@ -111,14 +95,12 @@ def plot_cells(state_vector: np.ndarray, d: float = 25):
     ax.set_xlim(0, d)
     ax.set_ylim(0, d)
     ar_scale = d / 250
-    n = np.size(state_vector) // 3
-    for i in range(n):
-        my_theta, my_x, my_y = state_vector[np.array([0, 1, 2]) + 3 * i]
+    for i in range(len(angles)):
         ax.arrow(
-            my_x,
-            my_y,
-            5 * ar_scale * np.cos(my_theta),
-            5 * ar_scale * np.sin(my_theta),
+            xy[0, i],
+            xy[1, i],
+            5 * ar_scale * np.cos(angles[i]),
+            5 * ar_scale * np.sin(angles[i]),
             head_width=2 * ar_scale,
             head_length=ar_scale,
             fc="k",
@@ -127,29 +109,12 @@ def plot_cells(state_vector: np.ndarray, d: float = 25):
     plt.draw()
 
 
-def update_animation(i: int, trajectory: np.ndarray, d: float):
-    """
-    Animation function for updating the plot.
-
-    Parameters
-    ----------
-    i : int
-        Frame index.
-    trajectory : np.ndarray
-        Trajectory of the cells.
-    d : float
-        Dimension of the space.
-    """
-    plot_cells(trajectory[:, i], d=d)
-
-
 def run_animation(
     n: int = 30,
-    t: int = 1000,
+    dt: float = 0.01,
     r: float = 1,
     d: float = 25,
     eta: float = 0.1,
-    stepsize: float = 1,
 ):
     """
     Run the animation of the Vicsek model.
@@ -169,22 +134,19 @@ def run_animation(
         Dimension of the space (default is 25).
     eta : float, optional
         Noise parameter (default is 0.1).
-    stepsize : float, optional
+    dt : float, optional
         Step size for the update (default is 1).
     """
-    state0 = initialize_cells(n, d=d)
-    trajectory = np.zeros(shape=(n * 3, t))
-    trajectory[:, 0] = state0
+    xy, positions = initialize_cells(n, d=d)
 
-    for i in range(t - 1):
-        trajectory[:, i + 1] = update_rule(
-            trajectory[:, i], stepsize=stepsize, eta=eta, r=r, d=d
-        )
+    def update_animation(frame: int):
+        nonlocal xy, positions
+        for _ in range(10):
+            xy, positions = update_rule(xy, positions, dt=dt, r=r, d=d, eta=eta)
+        plot_cells(xy, positions, d=d)
 
     fig, ax = plt.subplots()
-    ani = animation.FuncAnimation(
-        fig, update_animation, frames=t, fargs=(trajectory, d), interval=50
-    )
+    ani = animation.FuncAnimation(fig, update_animation, interval=1)
     plt.show()
 
 
